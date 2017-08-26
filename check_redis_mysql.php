67fba3ce0515d071dbfe7923692504c5fc92329e
wfy <1541123657@qq.com>
2017-08-27 00:00:53 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   check_redis_mysql.php

--- check_redis_mysql.php
+++ check_redis_mysql.php
@@ -1,5 +1,5 @@
 <?php 
-	$link = mysql_connect('localhost', 'mysql_user', 'mysql_password');
+	$link = mysql_connect('rm-uf66op2zv2435fr6z.mysql.rds.aliyuncs.com', 'root', 'WFY5714lxgk=3n');
 	if (!$link) {
 	    die('Could not connect: ' . mysql_error());
 	}
