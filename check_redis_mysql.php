9514dd8dfc25e896f8ed1dc826e0901806c692c4
wfy <1541123657@qq.com>
2017-08-27 01:02:04 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   check_redis_mysql.php

--- check_redis_mysql.php
+++ check_redis_mysql.php
@@ -3,6 +3,24 @@
 	if (!$link) {
 	    die('Could not connect: ' . mysql_error());
 	}
-	echo 'Connected successfully.' . '\n';
+	echo "Connected successfully.\n";
+
+	$db_selected = mysql_select_db('na_user', $link);
+	if (!$db_selected) {
+	    die ('Can\'t use foo : ' . mysql_error());
+	}
+	$query = "SELECT COUNT(*) FROM na_user";
+	// 执行查询
+	$result = mysql_query($query);
+
+	// 检查结果
+	// 下面显示了实际发送给 MySQL 的查询，以及出现的错误。这对调试很有帮助。
+	if (!$result) {
+	    $message  = 'Invalid query: ' . mysql_error() . "\n";
+	    $message .= 'Whole query: ' . $query;
+	    die($message);
+	}
+	print $result;
+	mysql_free_result($result);
 	mysql_close($link);
 ?>
